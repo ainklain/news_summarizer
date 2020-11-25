14ae156806975ef92319abca5d5c6d790b034e06
unknown <jeong-h.kim@sbc.sds.cloud.com>
2020-11-25 10:15:20 +0900

    date format fixed

--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+# python scraper.py --begin_d=20201101 --end_d=20201126
+
+
 import argparse
 import os
 import random
@@ -7,14 +10,22 @@ import datetime
 from selenium import webdriver
 from shutil import copyfile
 import json
-
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--begin_d', default=None, type=str)
 parser.add_argument('--end_d', default=None, type=str)
 parser.add_argument('--test', default=False, type=bool)
 parser.add_argument('--filedir', default='./out/naver_news', type=str)
+parser.add_argument('--metadir', default='./out/naver_news/meta', type=str)
 
+
+class TestArgs:
+    begin_d = None
+    end_d = None
+    test = True
+    filedir = './out/naver_news'
+    metadir = './out/naver_news/meta'
 
 
 def get_date_list(begin_d=None, end_d=None):
@@ -67,7 +78,7 @@ def get_url(base_url, mode, mid, oid, listType, date_):
         base_url=base_url, mode=mode, mid=mid, oid=oid, listType=listType, date_=date_)
 
 
-def get_articles_meta(cdriver, begin_d=None, end_d=None, test=False):
+def get_articles_meta(cdriver, begin_d=None, end_d=None, metadir='./out/naver_news/meta', test=False):
 
     """
     base url:
@@ -83,13 +94,27 @@ def get_articles_meta(cdriver, begin_d=None, end_d=None, test=False):
     oid_dict = get_oid()
     listType = 'paper'
     date_list = get_date_list(begin_d, end_d)
-    articles_meta = dict()
 
+    if not os.path.exists(metadir):
+        os.makedirs(metadir)
+
+    st_total = time.time()
     for date_ in date_list:
         s_t = time.time()
         time.sleep((0.2 + random.random()) * 3)
+
+        metapath = os.path.join(metadir, 'meta_{}.json'.format(date_))
+        if os.path.exists(metapath):
+            with open(metapath, 'rb') as f:
+                articles_meta = json.load(f)
+            print("[already exist] date={} | total_file={}".format(date_, len(articles_meta)))
+            continue
+        else:
+            articles_meta = dict()
+            print("date={} begin".format(date_))
+
         for oid, name in oid_dict.items():
-            print("{}: {} start".format(date_, name))
+            # print("{}: {} start".format(date_, name))
             url = get_url(base_url, mode, mid, oid, listType, date_)
 
             cdriver.get(url)
@@ -105,87 +130,116 @@ def get_articles_meta(cdriver, begin_d=None, end_d=None, test=False):
                     if id in articles_meta.keys():
                         continue
 
-                    articles_meta[id] = {'title':title,
+                    articles_meta[id] = {'title': title,
                                         'url': url,
                                         'date': date_,
                                         'from': name}
 
                     # 작동 테스트
                     if test and len(articles_meta) >= 2:
+                        with open(metapath, 'wb') as f:
+                            s = json.dumps(articles_meta, indent=4, ensure_ascii=False).encode('utf-8')
+                            f.write(s)
                         return articles_meta
 
-            print("{}: {} end".format(date_, name))
-        print("{} time spent: {} sec".format(date_, time.time() - s_t))
+            # print("{}: {} end".format(date_, name))
 
-    return articles_meta
+        with open(metapath, 'wb') as f:
+            s = json.dumps(articles_meta, indent=4, ensure_ascii=False).encode('utf-8')
+            f.write(s)
+
+        print("date={} | total_file={} | time spent: {:.3f} sec".format(date_, len(articles_meta), time.time() - s_t))
+
+    print('url scrapping done. {:.3f} sec'.format(time.time() - st_total))
 
 
-def scrap_news(cdriver, articles_meta, filedir='./out/naver_news', test=False):
+def scrap_news(cdriver, metadir='./out/naver_news/meta', filedir='./out/naver_news', test=False):
 
     if not os.path.exists(filedir):
         os.makedirs(filedir)
 
-    filename = 'news.json'
-    backup = 'news_backup.json'
-    log = 'log.log'
+    meta_list = os.listdir(metadir)
 
-    file_path = os.path.join(filedir, filename)
-    backup_path = os.path.join(filedir, backup)
+    log = 'log.log'
     log_path = os.path.join(filedir, log)
 
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as f:
-            articles = json.load(f)
-    else:
-        articles = dict()
+    st_total = time.time()
+    for meta_file in meta_list:
+        st_meta = time.time()
 
-    st = time.time()
-    for i, (id, article_meta) in enumerate(articles_meta.items()):
-        if test and i == 2:
-            break
+        date_ = ''.join(re.findall('[0-9]', meta_file))
 
-        if id in articles.keys():
-            continue
+        filename = 'news_{}.json'.format(date_)
+        backup = 'news_backup_{}.json'.format(date_)
 
-        cdriver.get(article_meta['url'])
-        cdriver.implicitly_wait(3)
-        try:
-            article_meta['content'] = cdriver.find_element_by_id('articleBodyContents').text
-            article_meta['class'] = cdriver.find_element_by_class_name('guide_categorization_item').text
-        except:
-            with open(log_path, 'a') as f:
-                str_ = '[failed] id: {} {} {}\n'.format(id, article_meta['title'], article_meta['url'])
-                f.write(str_)
+        meta_path = os.path.join(metadir, meta_file)
+        file_path = os.path.join(filedir, filename)
+        backup_path = os.path.join(filedir, backup)
 
-            continue
+        with open(meta_path, 'rb') as f:
+            articles_meta = json.load(f)
 
-        articles[id] = article_meta
-        with open(file_path, 'wb') as f:
-            s = json.dumps(articles, indent=4, ensure_ascii=False).encode('utf-8')
-            f.write(s)
+        if os.path.exists(file_path):
+            with open(file_path, 'rb') as f:
+                articles = json.load(f)
+        else:
+            articles = dict()
 
-        copyfile(file_path, backup_path)
-        time.sleep((0.2 + random.random()) * 5)
-        elapsed_time = time.time() - st
-        print("progress: {}/{} ({}%, expected {} sec)".format(i+1, len(articles_meta), (i+1)/len(articles_meta) * 100, elapsed_time * len(articles_meta) / (i+1)))
-    print('done')
+        st = time.time()
+        for i, (id, article_meta) in enumerate(articles_meta.items()):
+            if test and i == 2:
+                break
+
+            if id in articles.keys():
+                continue
+
+            cdriver.get(article_meta['url'])
+            cdriver.implicitly_wait(3)
+            try:
+                article_meta['content'] = cdriver.find_element_by_id('articleBodyContents').text
+                article_meta['class'] = cdriver.find_element_by_class_name('guide_categorization_item').text
+            except:
+                with open(log_path, 'a') as f:
+                    str_ = '[failed] date: {} id: {} {} {}\n'.format(date_, id, article_meta['title'], article_meta['url'])
+                    f.write(str_)
+
+                continue
+
+            articles[id] = article_meta
+            with open(file_path, 'wb') as f:
+                s = json.dumps(articles, indent=4, ensure_ascii=False).encode('utf-8')
+                f.write(s)
+
+            copyfile(file_path, backup_path)
+            time.sleep((0.2 + random.random()) * 5)
+            elapsed_time = time.time() - st
+            expected_total = elapsed_time * len(articles_meta) / (i+1)
+            print("date:{} progress: {}/{} ({:.3f}%, expected {:.3f} | remaining {:.3f} sec)".format(date_, i+1, len(articles_meta), (i+1)/len(articles_meta) * 100, expected_total, expected_total-elapsed_time))
+
+        print('date:{} done ({:.3f} sec)'.format(date_, time.time() - st_meta))
+    print('done ({:.3f} sec)'.format(time.time() - st_total))
+
 
 def main(args):
-
+    # args = TestArgs()
     cdriver = webdriver.Chrome('C:/chromedriver.exe')
     time.sleep(2)
 
-    # dictionary: key=news_id | value=[title, link]
-    # articles_meta = get_articles_meta(cdriver, begin_d='2020-11-02', end_d='2020-11-25')
-    articles_meta = get_articles_meta(cdriver, begin_d=args.begin_d, end_d=args.end_d, test=args.test)
-    with open(os.path.join(args.filedir, 'meta.json'), 'w') as f:
-        json.dump(articles_meta, f, indent=4)
+    if not os.path.exists(args.filedir):
+        os.makedirs(args.filedir)
 
-    time.sleep(2)
-    scrap_news(cdriver, articles_meta, filedir=args.filedir, test=args.test)
-    print('done')
-
-    cdriver.close()
+    try:
+        # dictionary: key=news_id | value=[title, link]
+        print('meta start')
+        get_articles_meta(cdriver, begin_d=args.begin_d, end_d=args.end_d, metadir=args.metadir, test=args.test)
+        print('meta done')
+        time.sleep(2)
+        print('scrap start')
+        scrap_news(cdriver, metadir=args.metadir, filedir=args.filedir, test=args.test)
+        print('scrap done')
+        cdriver.close()
+    except:
+        cdriver.close()
 
 
 if __name__ == '__main__':
